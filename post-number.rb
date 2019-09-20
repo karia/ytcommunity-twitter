76002d64633f66f9e5b06e1762c3f40dbc936387
karia <karia@side2.net>
2019-09-20 17:50:49 +0900

    チャンネル登録者数は差分があるときだけツイートする

--- post-number.rb
+++ post-number.rb
@@ -11,24 +11,33 @@ end
 Yt.configuration.api_key = ENV['YOUTUBE_API_KEY']
 channel_id = ARGV[0]
 
-#登録者数取得
+# 登録者数取得
 channel = Yt::Channel.new(id: channel_id)
-number = channel.subscriber_count
+num_after = channel.subscriber_count
 
-#URL組み立て
-item = "ミコちゃんのYouTubeチャンネル登録者数: " + number.to_s + " (" + Time.now.strftime("%m/%d %H:%M").to_s + "現在)"
+# 前回との差分を取得
+list_file = "/var/tmp/number_" + channel_id + ".txt"
+num_before = num_after
 
-#Tweetする
-if number >= 50000
-  client.update(item)
-  puts "[ツイートしました]" + item
-else
-  puts item
+File.open(list_file, "r") do |f| 
+  num_before = f.read.to_i
 end
 
-list_file = "/var/tmp/number_" + channel_id + ".txt"
+num_diff = num_after - num_before
+
+# 文言組み立て
+item = "ミコちゃんのYouTubeチャンネル登録者数: " + num_after.to_s + " (" + Time.now.strftime("%m/%d %H:%M").to_s + "現在 / 差分" + num_diff.to_s + ")"
+
+#Tweetする
+if num_diff == 0
+  puts item
+else
+  client.update(item)
+  puts "[ツイートしました]" + item
+end
+
 # 数字を保存
 File.open(list_file, "w") do |f| 
-  f.puts(number)
+  f.puts(num_after)
 end
 
