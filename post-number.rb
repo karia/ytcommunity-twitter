2484785c2a72b6ac89ad308d4ad67769cecc09c1
karia <karia@side2.net>
2019-09-24 20:52:25 +0900

    YouTubeチャンネル名を取得するように変更

--- post-number.rb
+++ post-number.rb
@@ -1,6 +1,7 @@
 require 'twitter'
 require 'yt'
 
+# API Keyを取得
 client = Twitter::REST::Client.new do |conf|
   conf.consumer_key = ENV['TWITTER_CONSUMER_KEY']
   conf.consumer_secret = ENV['TWITTER_CONSUMER_SECRET']
@@ -9,11 +10,14 @@ client = Twitter::REST::Client.new do |conf|
 end
 
 Yt.configuration.api_key = ENV['YOUTUBE_API_KEY']
+
+# configを取得
 channel_id = ARGV[0]
 
 # 登録者数取得
 channel = Yt::Channel.new(id: channel_id)
 num_after = channel.subscriber_count
+channel_name = channel.title
 
 # 前回との差分を取得
 list_file = "/var/tmp/number_" + channel_id + ".txt"
@@ -26,7 +30,7 @@ end
 num_diff = num_after - num_before
 
 # 文言組み立て
-item = "ミコちゃんのYouTubeチャンネル登録者数: " + num_after.to_s + " (" + Time.now.strftime("%m/%d %H:%M").to_s + "現在 / 差分" + num_diff.to_s + ")"
+item = "YouTubeチャンネル " + channel_name + " の登録者数: " + num_after.to_s + " (" + Time.now.strftime("%m/%d %H:%M").to_s + "現在 / 差分" + num_diff.to_s + ")"
 
 #Tweetする
 if num_diff == 0
